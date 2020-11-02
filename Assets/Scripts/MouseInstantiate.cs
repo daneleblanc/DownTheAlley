552c6dec0c5bb045dd469c94689a32f9b24a82a5
daneleblanc <daneleblanc4@gmail.com>
2020-11-02 00:16:56 -0600

    Bowling Pin Character & Active Ragdoll Bowling Pin Character Model Bowling Pin Character Rig Bowling Pin Animations Active Ragdoll that mimics dummy ragdoll Script for lauching balls for testing ragdoll

---
+++ Assets/Scripts/MouseInstantiate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInstantiate : MonoBehaviour
+{
+    public GameObject prefabToInstantiate;
+    public float speed = 15.0f;
+    public Camera targetCamera;
+
+    public void Awake()
+    {
+        //targetCamera = Camera.main;
+    }
+
+    public void Update()
+    {
+        if (prefabToInstantiate == null || targetCamera == null)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray mouseRay = targetCamera.ScreenPointToRay(Input.mousePosition);
+            GameObject newGameObject = (GameObject)Instantiate(prefabToInstantiate, mouseRay.origin, Quaternion.identity);
+            Rigidbody rb = newGameObject.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.velocity = mouseRay.direction * speed;
+            }
+        }
+    }
+}
